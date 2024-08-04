2e4c2a8622ffe85857b7e04abd30c4461595c03f
Rishav Kumar <rishavkumar208@gmail.com>
2024-08-05 00:42:48 +0530

    Feat(UI) : Add multiple seat selection while dragging

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import screen from "./assets/screen.svg";
 
 type Seat = {
@@ -14,6 +14,8 @@ type Seat = {
 
 function App() {
   const [selectedSeats, setSelectedSeats] = useState<Seat[]>([]);
+  const isMouseDown = useRef<boolean>(false);
+  const lastSelectedSeat = useRef<Seat | null>(null);
   //const [categories, setCategories] = useState<Category[]>([]);
 
   function handleSeatClick(row: number, col: number) {
@@ -30,13 +32,38 @@ function App() {
     console.log(selectedSeats);
   }
 
+  function handleMouseUp() {
+    isMouseDown.current = false;
+    lastSelectedSeat.current = null;
+  }
+  function handleMouseDown() {
+    isMouseDown.current = true;
+  }
+
+  function handleMouseMove(row: number, col: number) {
+    if (isMouseDown.current) {
+      if (
+        !lastSelectedSeat.current ||
+        lastSelectedSeat.current.row !== row ||
+        lastSelectedSeat.current.col !== col
+      ) {
+        lastSelectedSeat.current = { row, col };
+        handleSeatClick(row, col);
+      }
+    }
+  }
   return (
     <>
       <h1 className="text-3xl font-bold underline">Theator Creator</h1>
       <div className="flex flex-col items-center">
         <div className={`w-200 h-200 rounded-full cursor-pointer`}>
           <img src={screen} alt="screen" className="w-full h-full" />
-          <div className="flex flex-col space-y-2">
+          <div
+            className="flex flex-col space-y-2"
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+            onMouseLeave={handleMouseUp}
+          >
             {
               // 15 * 15 grid
               Array.from({ length: 15 }, (_, i) => (
@@ -53,6 +80,9 @@ function App() {
                       }`}
                       onClick={() => {
                         handleSeatClick(i, j);
+                      }}
+                      onMouseMove={() => {
+                        handleMouseMove(i, j);
                       }}
                     ></div>
                   ))}
