665e020cb0bda84910d53494e3f89fa65a9284a3
Rishav Kumar <rishavkumar208@gmail.com>
2024-08-08 15:14:57 +0530

    Fix(sql): spelling mistake

--- src/sql/schema/002_movies.sql
+++ src/sql/schema/002_movies.sql
@@ -6,8 +6,14 @@ CREATE TABLE movies (
     duration INT NOT NULL, -- duration in minutes
     language VARCHAR(50),
     genre VARCHAR(255),
+    start_date DATE,
+    end_date DATE,
     release_date DATE,
     rating DECIMAL(2,1), -- average rating
+    image VARCHAR(255),
+    directors VARCHAR(255)[],
+    actors VARCHAR(255)[],
+    trailer VARCHAR(255),
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
