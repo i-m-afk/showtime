6f035df3ecc541845da2625d9a62f87a75373a48
Rishav Kumar <rishavkumar208@gmail.com>
2024-08-17 01:17:37 +0530

    fix(schema): removed id in booking_details
    id for this is not required as each booking_details
    can be uniquely identified by the seat number and
    booking id.

--- src/sql/schema/008_booking_details.sql
+++ src/sql/schema/008_booking_details.sql
@@ -1,6 +1,5 @@
 -- +goose Up
 CREATE TABLE booking_details (
-    id UUID PRIMARY KEY,
     booking_id UUID NOT NULL REFERENCES bookings(id) ON DELETE CASCADE,
     seat_id INT NOT NULL REFERENCES seats(seat_number) ON DELETE CASCADE,
     price DECIMAL(5, 2) NOT NULL,
