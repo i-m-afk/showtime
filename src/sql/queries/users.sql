21bffce8f09be6dc5680f1dfe21ecf04963344c5
Rishav Kumar <rishavkumar208@gmail.com>
2024-08-17 12:05:52 +0530

    Fix(schema): user sql removed order id

--- src/sql/queries/users.sql
+++ src/sql/queries/users.sql
@@ -2,6 +2,6 @@
 SELECT * FROM users WHERE id = $1;
 
 -- name: CreateUser :one
-INSERT INTO users (id, username, name, email, role, phone, order_id, password)
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+INSERT INTO users (id, username, name, email, role, phone, password)
+VALUES ($1, $2, $3, $4, $5, $6, $7)
 returning *;
